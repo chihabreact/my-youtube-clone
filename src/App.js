0395d3e3a650479be962291230ce6de4321dcbcb
chihabreact <bouzar866@gmail.com>
2023-08-27 23:45:58 +0000

    comps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
-
-const App = () => {
-  return <div>hello</div>;
-};
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Feed,
+  ChannelDetail,
+  SearchFeed,
+  VideoDetail,
+  NavBar,
+} from "./Components";
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <NavBar />
+      <Routes>
+        <Route path="/" excat element={<Feed></Feed>} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
